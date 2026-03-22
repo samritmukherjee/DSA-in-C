e292ba6f2a6a362a700c882d90a1a64679f05418
Samrit Mukherjee <samritmukherjee05@gmail.com>
2026-03-22 19:57:07 +0530

    Call By Reference

---
+++ CallByReference.c
@@ -0,0 +1,23 @@
+#include <stsdio.h>
+void swap(int *a, int *b)
+ {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+ }
+
+ int main()
+  {
+    int x, y;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+ }
